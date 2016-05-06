3d4c391e5e0cfd0d40667a4272ea1e25bf232bb7
Santiago Castano <santiago.castanom@gmail.com>
2016-05-06 18:49:31 -0500

    Initial Commit

---
+++ MyOwnApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOwnApp1
+//
+//  Created by Santiago Castaño M on 5/6/16.
+//  Copyright © 2016 LearnAPps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
