d6eecd1afaa0562bf1d552a15c07c1de69cc2268
Santiago Castaño M <santiagocastano@Santiagos-MacBook-Air-2.local>
2016-05-08 00:25:30 -0500

    2nd commit

--- MyOwnApp1/ViewController.swift
+++ MyOwnApp1/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var oldLogo: UIImageView!
+    @IBOutlet weak var newLogo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func toggleOldButton(sender: AnyObject) {
+        if oldLogo.hidden == false{
+            oldLogo.hidden = true
+        }
+        else{
+            oldLogo.hidden = false
+        }
+        
+    }
+    
+    @IBAction func toggleNewButton(sender: AnyObject) {
+        if newLogo.hidden == false{
+            newLogo.hidden = true
+        }
+        else{
+            newLogo.hidden = false
+        }
+        
+    }
+    
 }
 
